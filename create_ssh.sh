9d7cb579a29f62db382fcdf0d488f02ed9a15ec5
ruka <lukas.figueiredo@hotmail.com>
2022-04-19 17:36:32 -0300

    Update create_ssh.sh

--- create_ssh.sh
+++ create_ssh.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Generate codeship_deploy_key and codeship_deploy_key.pub, configured to not require passphrase
-docker run -it --rm -v $(pwd):/keys/ codeship/ssh-helper generate "<YOUR_EMAIL>"
+docker run -it --rm -v $(pwd):/keys/ codeship/ssh-helper generate "lukas.figueiredo@hotmail.com"
 
 # Store codeship_deploy_key as a one line entry in codeship.env file under `PRIVATE_SSH_KEY`
 docker run -it --rm -v $(pwd):/keys/ codeship/ssh-helper prepare
