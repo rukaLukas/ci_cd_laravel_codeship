7803f794ae4fb8f68a27f852af062d07ca65dbad
lucas figueiredo <lukas.figueiredo@hotmail.com>
2022-04-26 16:58:25 -0300

    new test

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-rsync -avz ./ root@137.184.144.59:/var/www/backend/ci_cd_laravel_codeship
+rsync -avz ./Dockerfile root@137.184.144.59:/var/www/backend/ci_cd_laravel_codeship
 docker-compose up --build
